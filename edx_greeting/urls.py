13c12b4ac4c008bb0edef67cfce584449086f43e
molchoun <ne.molchoun@gmail.com>
2023-08-28 21:22:57 +0400

    initial app setup

--- edx_greeting/urls.py
+++ edx_greeting/urls.py
@@ -3,7 +3,7 @@ URLs for edx_greeting.
 """
 from django.urls import re_path  # pylint: disable=unused-import
 from django.views.generic import TemplateView  # pylint: disable=unused-import
-from django.urls import path
+from django.urls import path, include
 from edx_greeting import views
 from django.views.decorators.csrf import csrf_exempt
 
@@ -12,5 +12,11 @@ urlpatterns = [
     # re_path(r'', TemplateView.as_view(template_name="edx_greeting/base.html")),
     path('', views.login, name='index'),
     path('authorize/', views.authorize),
-    path('greeting/', csrf_exempt(views.GreetingView.as_view()))
+    path('greeting/', csrf_exempt(views.GreetingView.as_view())),
+    path("o/", include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
+
+
+# {
+# "text": "hello"
+# }
